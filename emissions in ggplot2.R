76f0f36f3ca8ba8aea29464cb183513907cbc3fd
ttkn <ttkn@users.noreply.github.com>
2015-08-21 13:09:39 -0700

    code for 6 plots

--- emissions in ggplot2.R
+++ emissions in ggplot2.R
@@ -35,3 +35,49 @@ png(filename = "plot3.png", width = 480, height = 480)
  ggplot(data=baltimore, aes(year, pm2.5)) + geom_line(aes(color=type)) + labs(title="Annual PM2.5 Emissions in Baltimore", y="emissions in tons") 
  dev.off()
  
+#######plot4
+ 
+x <- filter(df2, grepl("coal", df2$EI.Sector, ignore.case=TRUE))
+
+coal <- data.frame(df %>% 
+    filter(SCC %in% x$SCC) %>% 
+    group_by(year) %>% 
+    summarize(pm2.5 = sum(Emissions)))
+
+png(filename = "plot4.png", width = 480, height = 480)
+  ggplot(data=qwer, aes(year, pm2.5)) +
+    geom_bar(stat="identity") +
+    labs(title="U.S. emissions from coal, 1999-2008", y="emissions in tons") +
+    scale_x_continuous(breaks=coal$year, labels=coal$year)
+  dev.off()
+
+######plot5
+x <- filter(df2, grepl("vehicle", df2$EI.Sector, ignore.case = TRUE))
+
+cars <- data.frame(df %>%
+    filter(SCC %in% x$SCC & fips == 24510) %>%
+    group_by(year) %>%
+    summarize(pm2.5 = sum(Emissions)))
+
+png(filename = "plot5.png", width = 480, height = 480)
+  ggplot(data=cars, aes(year, pm2.5)) +
+    geom_bar(stat="identity") +
+    labs(title="Vehicle emissions in Baltimore, 1999-2008", y="emissions in tons") +
+    scale_x_continuous(breaks=coal$year, labels=coal$year)
+dev.off()
+
+###plot6
+x <- filter(df2, grepl("vehicle", df2$EI.Sector, ignore.case = TRUE))
+
+cars <- data.frame(df %>%
+                     filter(SCC %in% x$SCC, fips %in% c("06037", 24510)) %>%
+                     group_by(fips, year) %>%
+                     summarize(pm2.5 = sum(Emissions)))
+
+png(filename = "plot6.png", width = 480, height = 480)
+ggplot(data=cars, aes(year, pm2.5)) +
+  geom_bar(stat="identity", aes(colors=fips)) +
+  labs(title="Vehicle emissions in Baltimore and Los Angeles, 1999-2008", y="emissions in tons") +
+  scale_x_continuous(breaks=cars$year, labels=cars$year) +
+  facet_wrap(~fips)
+dev.off()
